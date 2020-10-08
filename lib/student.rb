6fa1e151858520473de772fc4cfd05cae1fc9524
Brock Byrd <brockbyrdd@gmail.com>
2020-10-08 15:08:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,11 +38,13 @@ class Student
   end
 
   def self.create
-
+    sql = <<-SQL
+      INSERT INTO students VALUES (name, grade)
+    SQL
   end
 
   def self.new_from_db
-
+    
   end
 
   def self.find_by_name
